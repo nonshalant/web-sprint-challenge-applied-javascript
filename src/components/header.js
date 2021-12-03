c27e1abacb6b6b04f9140cfa0e06919dffc1cb83
Shamir Gibbons <nonshalant@shamirs-air.myfiosgateway.com>
2021-12-03 09:00:03 -0500

    update on the sprint

--- src/components/header.js
+++ src/components/header.js
@@ -10,7 +10,25 @@ const Header = (title, date, temp) => {
   //    <h1>{ title }</h1>
   //    <span class="temp">{ temp }</span>
   //  </div>
-  //
+
+  const headerDiv = document.createElement('div');
+  const createSpan1 = document.createElement('span');
+  const createH1 = document.createElement('h1');
+  const createSpan2 = document.createElement('span');
+
+  headerDiv.appendChild(createSpan1);
+  headerDiv.appendChild(createH1);
+  headerDiv.appendChild(createSpan2);
+
+  headerDiv.classList.add('header');
+  createSpan1.classList.add('date');
+  createSpan2.classList.add('temp');
+  
+  createSpan1.textContent = date;
+  createH1.textContent = title;
+  createSpan2.textContent = temp;
+
+  return headerDiv;
 }
 
 const headerAppender = (selector) => {
@@ -19,7 +37,10 @@ const headerAppender = (selector) => {
   // Implement this function taking a css selector as its only argument.
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
-  //
+  const entryPoint = document.querySelector(selector);
+  entryPoint.appendChild(Header('Lambda Times','January 6 2021','26'));
+  return entryPoint
+
 }
 
 export { Header, headerAppender }
--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const Tabs = (topics) => {
   // TASK 3
   // ---------------------
@@ -12,7 +14,26 @@ const Tabs = (topics) => {
   //   <div class="tab">bootstrap</div>
   //   <div class="tab">technology</div>
   // </div>
-  //
+  const topicsDiv = document.createElement('div');
+  const tabDiv1 = document.createElement('div');
+  const tabDiv2 = document.createElement('div');
+  const tabDiv3 = document.createElement('div');
+
+  topicsDiv.classList.add('topics');
+  tabDiv1.classList.add('tab');
+  tabDiv2.classList.add('tab');
+  tabDiv3.classList.add('tab');
+
+  tabDiv1.textContent = topics[0];
+  tabDiv2.textContent = topics[1];
+  tabDiv3.textContent = topics[2];
+
+  topicsDiv.appendChild(tabDiv1);
+  topicsDiv.appendChild(tabDiv2);
+  topicsDiv.appendChild(tabDiv3);
+
+  return topicsDiv;
+
 }
 
 const tabsAppender = (selector) => {
@@ -23,6 +44,18 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+  axios.get(`http://localhost:5000/api/topics`)
+  .then(resp =>{
+    const topicsArr = resp.data.topics;
+    Tabs(topicsArr)
+    const entryPoint = document.querySelector(selector);
+    return entryPoint
+  })
+  .catch(err =>{
+    console.log('sorry we got an error!');
+    console.error(err)
+  })
+
 }
 
 export { Tabs, tabsAppender }
